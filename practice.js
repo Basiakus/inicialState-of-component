a5819a4eb0d252fe1d06b63c094f761884a2d688
Adam Basiak <basiakus@gmail.com>
2017-12-11 19:52:19 +0100

    put preState as argument

--- practice.js
+++ practice.js
@@ -6,33 +6,43 @@ var Counter = React.createClass({
     },
 
     increment: function() {
-        this.setState({
-            counter: this.state.counter + 1 
+        this.setState(function(prevState) {
+           return {
+              counter: prevState.counter + 1
+           };
         });
         console.log('+1')
     },
     decrement: function() {
-        this.setState ({
-            counter: this.state.counter - 1
+        this.setState(function(prevState) {
+           return {
+              counter: prevState.counter - 1
+           };
         });
         console.log('-1')
     },
-    multi: function() {
-        this.setState ({
-            counter: this.state.counter * 3
+   multi: function() {
+         this.setState(function(prevState) {
+           return {
+              counter: prevState.counter * 3
+           };
         });
         console.log('*3')
     },
     div: function() {
-        this.setState ({
-            counter: this.state.counter / 3
+        this.setState(function(prevState) {
+           return {
+              counter: prevState.counter / 3
+           };
         });
         console.log('/3')
     },
     resetCounter: function() {
-    	this.setState ({
-    		counter: this.state.counter = 0
-    	});
+    	this.setState(function(prevState) {
+           return {
+              counter: prevState.counter = 0
+           };
+        });
     	console.log('reset')
     },
 
@@ -52,28 +62,34 @@ var Count = React.createClass({
 
     getInitialState: function() {
         return {
-            count: 100
+            count: 10
         };
     },
 
     multiTen: function() {
-        this.setState ({
-            count: this.state.count * 10
+        this.setState(function(prevState) {
+           return {
+              count: prevState.count * 10
+           };
         });
         console.log('*10')
     },
 
     divTen: function() {
-        this.setState ({
-            count: this.state.count / 10
+        this.setState(function(prevState) {
+           return {
+              count: prevState.count / 10
+           };
         });
         console.log('/10')
     },
 
     resetCount: function() {
-    	this.setState ({
-    		count: this.state.counter = 0
-    	});
+    	this.setState(function(prevState) {
+           return {
+              count: prevState.count = 0
+           };
+        });
     	console.log('reset')
     },
 
